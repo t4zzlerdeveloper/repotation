59b194a060770bd395973d03425ebd15377b2b0d
t4zzlerdeveloper <38925744+t4zzlerdeveloper@users.noreply.github.com>
2024-01-25 12:13:50 +0000

    testing github api

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,47 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import viteLogo from '/vite.svg'
+import axios from 'axios';
 
 function App() {
+
+  const [url,setURL] = useState("");
+  const [stats,setStats] = useState([]);
+
+  function search(url){
+
+    if(url.startsWith("https://github.com/")){
+      const gitPath = url.replace("https://github.com/","");
+      if(gitPath.split('/')[1]){
+        const fullRepo = gitPath;
+        const username = fullRepo.split('/')[0];
+        const repoName = fullRepo.split('/')[1];
+        console.log("\n------------------")
+        console.log("fullRepo: " + fullRepo);
+        console.log("username: " + username);
+        console.log("repoName: " + repoName);
+      }
+      else{
+        const username = gitPath.replace("/","");
+        console.log("\n------------------")
+        console.log("username: " + username);
+        fetchUser(username);
+      }
+
+    }
+    else{
+      alert("Invalid GitHub URL")
+    }
+  }
+
+  function fetchUser(username){
+    axios.get("https://api.github.com/users/" + username)
+    .then((res)=>{
+      setStats(res.data)
+    })
+    .catch(()=>{
+      //setStats([]);
+    })
+  }
 
   return (
     <>
@@ -9,19 +49,23 @@ function App() {
         <h1>repotation</h1>
 
         <section className='card search-bar'>
-          <input className={""} placeholder={"Enter GitHub URL or Repo Name"} />
-          <button>Search</button>
+          <input className={""} placeholder={"Enter GitHub URL or Repo Name"} value={url} onChange={(e)=>{setURL(e.target.value)}} />
+          <button onClick={()=>{search(url)}}>Search</button>
         </section>
 
         <section className='card'>
-          <div className='git-stats'>
+          {
+            stats.length === 0 ? <>Loading...</>:
+            <div className='git-stats'>
             <div className='git-left'>
-              <img src={viteLogo}/>
+              <img src={stats.avatar_url}/>
             </div>
             <div className='git-right'>
-              Hello world
+              <h1>{stats.login}</h1>
+              <p>{stats.bio}</p>
             </div>
           </div>
+          }
         </section>
 
       </div>
